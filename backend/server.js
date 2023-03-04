852b52fcbc8533906aff5e7b8a9f4a6bac187481
Prajwal Kuchewar <prajwalkuchewar3@gmail.com>
2023-03-05 01:12:57 +0530

    Gitignore modified

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,11 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
-const port = 5000;
 
-// Use bodyParser middleware to parse JSON body of POST request
-app.use(bodyParser.json());
-
-// Define a route to handle POST request
-app.post('/contact', (req, res) => {
-	const { name, email, message } = req.body;
-
-	// Do something with the data received from the client
-	console.log(`Received message from ${name} <${email}>: ${message}`);
-
-	// Send a response back to the client
-	res.send('Message received!');
+app.get('/api', (req, res) => {
+	res.json({ users: ['UserOne', 'UserTwo', 'UserThree'] });
 });
 
-// Start the server
-app.listen(port, () => {
-	console.log(`Server listening on port ${port}`);
+app.listen(5000, () => {
+	console.log('Server started on port 5000');
 });
